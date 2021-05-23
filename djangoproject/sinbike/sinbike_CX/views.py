833f28f9bd7542d4fe2279434c4fe613a1c5adcc
youtube-dm94 <63930715+youtube-dm94@users.noreply.github.com>
2021-05-23 08:14:42 +0800

    210523
    Error with database

--- djangoproject/sinbike/sinbike_CX/views.py
+++ djangoproject/sinbike/sinbike_CX/views.py
@@ -1,10 +1,12 @@
 from django.core import paginator
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
-from .models import Question
+from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def main(request):
     """
@@ -26,6 +28,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'sinbike_CX/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     Answer
@@ -34,23 +37,26 @@ def answer_create(request, question_id):
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
-            question = form.save(commit=False)
-            question.create_date = timezone.now()
-            question.save()
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.create_date = timezone.now()
+            answer.save()
             return redirect('support:detail', question_id=question.id)
     else:
         form = AnswerForm()
     context = {'question':question, 'form':form}
     return render(request, 'sinbike_CX/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     """
-    Create
+    Create Ticket
     """
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('support:main')
@@ -58,3 +64,61 @@ def question_create(request):
         form = QuestionForm()
     context = {'form':form}
     return render(request, 'sinbike_CX/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+    Edit Ticket
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, 'Not Authorized')
+        return redirect('support:main', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('support:main', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form':form}
+    return render(request, 'sinbike_CX/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """
+    Delete Ticket
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, 'Not Authorized')
+        return redirect('support:main', question_id=question.id)
+    question.delete()
+    return redirect('support:main')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    """
+    Edit Answer
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, 'Not Authorized')
+        return redirect('support:main', question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('support:main', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'sinbike_CX/answer_form.html', context)
