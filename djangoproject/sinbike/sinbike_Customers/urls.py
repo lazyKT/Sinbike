0426c67a12d01e425d46c5b880973716f8b78789
lazyKT <kyaw.thitlwin.me@gmail.com>
2021-07-13 06:18:53 +0800

    Transaction Model and CRUD Done

--- djangoproject/sinbike/sinbike_Customers/urls.py
+++ djangoproject/sinbike/sinbike_Customers/urls.py
@@ -6,7 +6,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('', views.CustomerListView.as_view(), name='customers'),
-    path ('<int:id>', views.CustomerDetail.as_view(), name='customers'),
-    path ('transactions/', views.TransactionListView.as_view(), name='transactions')
+    path ('', views.CustomerListView.as_view(), name='customers'), # customer list
+    path ('<int:id>', views.CustomerDetailListView.as_view(), name='customers'), # single customer
+    path ('transactions/', views.TransactionListView.as_view(), name='transactions'), # transaction list
+    path ('transactions/<int:id>', views.TransactionDetailListView.as_view(), name='transactions') # single transaction
 ]
