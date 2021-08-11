180792916358b487c64eb34a32bb47e5ba289237
youtube-dm94 <63930715+youtube-dm94@users.noreply.github.com>
2021-08-12 04:59:21 +0800

    210812 update_reCAPTCHA

--- djangoproject/sinbike/common/forms.py
+++ djangoproject/sinbike/common/forms.py
@@ -10,26 +10,6 @@ from django.contrib.auth.models import User
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="Email")
     
-
     class Meta:
         model = User
-        fields = ("username", "email")
-
-    def clean(self):
-        # Google reCAPTCHA
-        recaptcha_response = self.request.POST.get('g-recaptcha-response')
-        url = 'https://www.google.com/recaptcha/api/siteverify'
-        values = {
-            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-            'response': recaptcha_response
-        }
-        data = urllib.parse.urlencode(values).encode()
-        req = urllib.request.Request(url, data=data)
-        response = urllib.request.urlopen(req)
-        result = json.loads(response.read().decode())
-        ''' reCAPTCHA done '''
-
-        if not result['success']:
-            raise forms.ValidationError(_('reCAPTCHA error occurred.'))
-
-        return super(UserForm, self).clean()
+        fields = ("username", "password1", "password2", "email")
--- djangoproject/sinbike/common/views.py
+++ djangoproject/sinbike/common/views.py
@@ -1,5 +1,10 @@
+import urllib
+import json
+
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
 from .forms import UserForm
 from .models import FAQ
 
@@ -11,14 +16,35 @@ def signup(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+
+            # Google reCAPTCHA
+            ''' reCAPTCHA starts '''
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            values = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            data = urllib.parse.urlencode(values).encode()
+            req = urllib.request.Request(url, data=data)
+            response = urllib.request.urlopen(req)
+            result = json.loads(response.read().decode())
+            ''' reCAPTCHA done '''
+
+            if result['success']:
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid reCAPTCHA. Please try again.')
+
+            return redirect('common:signup')
     else:
         form = UserForm()
+
     return render(request, 'common/signup.html', {'form': form})
 
 def faq(request):
